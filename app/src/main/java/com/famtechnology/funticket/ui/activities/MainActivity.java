edfc3f7d5a039bbdfe74ab8225cb2643662e7d4a
Felipe Luna <luiz.silva@qiwi.com.br>
2020-03-10 11:37:59 -0300

    Criação Das Classes Base

---
+++ app/src/main/java/com/famtechnology/funticket/ui/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.famtechnology.funticket.ui.activities;
+
+import com.famtechnology.funticket.R;
+import com.famtechnology.funticket.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    public int setView() throws Exception {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void setupData() throws Exception {
+
+    }
+}
